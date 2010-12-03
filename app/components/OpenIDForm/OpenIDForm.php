a89e61e17e4190e71766869ddfc501cc1b2ddcba
StanleyK <stanleyk@stanleyk.net>
2010-12-03 10:39:06 +0100

    Doplneni phpDoc dokumentace, drobne upravy v CSS

--- app/components/OpenIDForm/OpenIDForm.php
+++ app/components/OpenIDForm/OpenIDForm.php
@@ -1,29 +1,66 @@
 <?php
 
+/**
+ * OpenID Authentication Form
+ *
+ * Copyright (c) 2010 Stanislav Kocanda (http://www.stanleyk.net)
+ *
+ * License: MIT
+ *
+ */
+
 namespace OpenIDForm;
 
 use Nette;
 use Nette\Debug;
 use Nette\Application\AppForm;
 
+/**
+ * OpenID Authentication Form control
+ *
+ * @author     Stanislav Kocanda
+ */
 class OpenIDForm extends Nette\Application\Control
 {
+	/** form field name */
 	const OID_FIELD = 'openid_identifier';
+
+	/** name of the signal processing OP redirection */
 	const PROCESS_SIGNAL = 'process';
 
+	/** @var array of function(void);
+	 *  Occurs when the user cancels OpenID authentication */
 	public $onCancel;
+
+	/** @var array of function(void);
+	 *  Occurs when the user authentication fails */
 	public $onInvalid;
+
+	/** @var array of function($identity, $attributes);
+	 *  Occurs when the user authentication succeeds */
 	public $onValid;
 
+	/** @var LightOpenID */
 	protected $openid;
 
-	public function __construct( Nette\IComponentContainer $parent = NULL, $name = NULL )
-	{
+	/**
+	 * Component constructor.
+	 * @param  Nette\IComponentContainer
+	 * @param  string
+	 */
+	public function __construct(
+		Nette\IComponentContainer $parent = NULL, $name = NULL
+	) {
 			parent::__construct( $parent, $name );
 
 			$this->openid = new \LightOpenID;
 	}
 
+	/**
+	 * Sets required fields in the AX format.
+	 * Accepts either string or an array of strings
+	 * @param  mixed 
+	 */
 	public function setRequired( $required ) {
 		if ( is_array( $required ) ) {
 			$this->openid->required = array_merge(
@@ -36,6 +73,11 @@ class OpenIDForm extends Nette\Application\Control
 		}
 	}
 
+	/**
+	 * Sets optional fields in the AX format.
+	 * Accepts either string or an array of strings
+	 * @param  mixed 
+	 */
 	public function setOptional( $optional ) {
 		if ( is_array( $optional ) ) {
 			$this->openid->optional = array_merge(
@@ -48,27 +90,40 @@ class OpenIDForm extends Nette\Application\Control
 		}
 	}
 
-	public function render()
-	{
+	/**
+	 * Component rendering
+	 */
+	public function render() {
 		$template = $this->template;
 		$template->setFile(__DIR__ . '/default.phtml');
 		$template->render();
 	}
 
+	/**
+	 * Handle the process! signal.
+	 * This signal occurs after a successful redirection back from the OP
+	 */
 	public function handleProcess() {
 		$this->openid->returnUrl = $this->returnUrl( self::PROCESS_SIGNAL );
 		if( $this->openid->mode == 'cancel') {
 			$this->onCancel();
-		} elseif( $this->openid->mode ) {
+		}
+		elseif( $this->openid->mode ) {
 			if ( $this->openid->validate() ) {
 				$this->onValid(
 					$this->openid->identity,
 					$this->openid->getAttributes()
 				);
 			}
+			else {
+				$this->onInvalid();
+			}
 		}
 	}
 
+	/**
+	 * Create the form itself
+	 */
 	public function createComponentIdentifierForm() {
 		$form = new AppForm;
 		$form->addText( self::OID_FIELD, 'Sign in with OpenID:')
@@ -78,8 +133,11 @@ class OpenIDForm extends Nette\Application\Control
 		return $form;
 	}
 
-	public function processIdentifier( AppForm $form )
-	{
+	/**
+	 * The form onSubmit function
+	 * @param AppForm
+	 */
+	public function processIdentifier( AppForm $form ) {
 		$values = $form->values;
 		$this->openid->identity = $values[ self::OID_FIELD ];
 		$this->openid->returnUrl = $this->returnUrl( self::PROCESS_SIGNAL );
@@ -88,6 +146,11 @@ class OpenIDForm extends Nette\Application\Control
 		exit();
 	}
 
+	/**
+	 * Compose the return URL for the OpenId request.
+	 * Includes the processing signal call.
+	 * @param string
+	 */
 	protected function returnUrl( $signal ) {
 		return $this->openid->trustRoot .
 			$this->getParent()->link( $this->getName() . ':' . $signal . '!' );
--- app/presenters/DefaultPresenter.php
+++ app/presenters/DefaultPresenter.php
@@ -6,18 +6,20 @@
  * @copyright  Copyright (c) 2010 Stanislav Kocanda
  */
 
+use \Nette;
+use \OpenIDForm;
 
 /**
  * Default presenter.
  *
  * @author     Stanislav Kocanda
  */
-
-use \Nette;
-use \OpenIDForm;
-
 final class DefaultPresenter extends Nette\Application\Presenter
 {
+	/**
+	 * OpenID Form control factory.
+	 * @return mixed
+	 */
 	public function createComponentOpenIDForm()
 	{
 		$oid = new \OpenIDForm\OpenIDForm();
@@ -29,16 +31,27 @@ final class DefaultPresenter extends Nette\Application\Presenter
 		return $oid;
 	}
 
+	/**
+	 * Successful login callback.
+	 * @param string
+	 * @param array
+	 */
 	public function validOpenID( $identity, $attributes ) {
 		$this->template->msg = 
 			'You have successfuly logged in as ' . $identity;
 		$this->template->attributes = $attributes;
 	}
 
+	/**
+	 * Failed login callback.
+	 */
 	public function invalidOpenID() {
 		$this->template->msg = 'You have not logged in!';
 	}
 
+	/**
+	 * Cancelled login callback.
+	 */
 	public function cancelledOpenID() {
 		$this->template->msg = 'You have cancelled logging in!';
 	}
